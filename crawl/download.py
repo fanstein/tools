a5b6bdadbdda2f17258fa79c38d7600bbc7a51b9
fanstein <fanfly1607@163.com>
2018-06-29 22:32:00 +0800

    aa

--- crawl/download.py
+++ crawl/download.py
@@ -1,17 +1,59 @@
+import re
+import itertools
 import requests
 import urllib2
+import urlparse
+
 
 # http://httpstat.us
 
 
-def download(url, num_retries=2):
+def download(url, num_retries=2, user_agent='wswp'):
     print 'downloading:', url
+    headers = {'User-agent': user_agent}
+    request = urllib2.Request(url, headers=headers)
     try:
-        html = urllib2.urlopen(url).read()
+        html = urllib2.urlopen(request).read()
     except urllib2.URLError as e:
         print 'download error:', e.reason
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code <= 600:
-                return download((url, num_retries - 1))
+                return download(url, num_retries - 1)
     return html
+
+
+def crawl_sitemap(url):
+    sitemap = download(url)
+    links = re.findall('<loc>(.*?)</loc>',sitemap)
+    for link in links:
+        html = download(link)
+
+
+def crawl_id():
+    for page in itertools.count(1):
+        url = 'http://example.webscraping.com/view/-%d'%page
+        html = download(url)
+        if html is None:
+            break
+        else:
+            pass
+
+
+def get_links(html):
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']',re.IGNORECASE)
+    return webpage_regex.findall(html)
+
+
+def link_crawler(seed_url, link_regex):
+    crawl_queue = [seed_url]
+    seen = set(crawl_queue)
+    while crawl_queue:
+        url = crawl_queue.pop()
+        html = download(url)
+        for link in get_links(html):
+            if re.match(link_regex, link):
+                link = urlparse.urljoin(seed_url, link)
+                if link not in seen:
+                    seen.add(link)
+                    crawl_queue.append(link)
